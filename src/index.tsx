6e635ccb11a4c798817ec9e7085e11f4c634c41e
plou102 <plou102@naver.com>
2023-09-10 17:22:16 +0900

    Feat: 절대경로 설정 및 라우터 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App';
+import Main from '@/pages/Main';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        path: '/',
+        element: <Main />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<RouterProvider router={router} />);
